3cc8333c966f2809adc760b8f56775e5e155db73
abhinav <abhinavsharma24999@gmail.com>
2019-10-13 23:43:16 +0530

    UI enhanced

--- app/src/main/java/com/example/ieeegbpec/Fragments/EventsFragment.java
+++ app/src/main/java/com/example/ieeegbpec/Fragments/EventsFragment.java
@@ -1,6 +1,7 @@
 package com.example.ieeegbpec.Fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import com.example.ieeegbpec.R;
 
@@ -24,6 +26,12 @@ public class EventsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        
+        CardView cardView8 = view.findViewById(R.id.card8);
+        cardView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Seminar on Android Application Development by ABHINAV SHARMA", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
