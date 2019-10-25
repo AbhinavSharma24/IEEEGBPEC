b628a00405ca9055f4691fb018902d2c34cc1db5
abhinav <abhinavsharma24999@gmail.com>
2019-10-25 23:46:55 +0530

    UI enhanced

--- app/src/main/java/com/example/ieeegbpec/Fragments/IEEEResourcesFragment.java
+++ app/src/main/java/com/example/ieeegbpec/Fragments/IEEEResourcesFragment.java
@@ -1,5 +1,30 @@
 package com.example.ieeegbpec.Fragments;
 
-public class IEEEResourcesFragment {
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ieeegbpec.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IEEEResourcesFragment extends Fragment {
+
+
+    public IEEEResourcesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ieeeresources, container, false);
+    }
 
 }
