392956314d1577bdf183b5553a2de79176850c78
abhinav <abhinavsharma24999@gmail.com>
2019-10-30 23:07:07 +0530

    UI enhanced

---
+++ app/src/main/java/com/example/ieeegbpec/Activities/EventActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ieeegbpec.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ieeegbpec.R
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+}
