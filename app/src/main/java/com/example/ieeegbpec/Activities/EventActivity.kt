c063d6a73465cae8c79d3042a850730530f95e1f
abhinav <abhinavsharma24999@gmail.com>
2019-10-31 14:11:15 +0530

    UI enhanced

--- app/src/main/java/com/example/ieeegbpec/Activities/EventActivity.kt
+++ app/src/main/java/com/example/ieeegbpec/Activities/EventActivity.kt
@@ -3,14 +3,24 @@ package com.example.ieeegbpec.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ieeegbpec.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_event.*
 
 class EventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        Picasso.get().load("http://ourpolitics.net/wp-content/uploads/2017/12/events.jpg")
+            .fit().centerCrop().into(toolbarImg)
 
-
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
